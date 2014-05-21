329658e050e8de1b36d1ee2e45cd9d61ef4fb7b3
Lens-F <tnoah12@gmail.com>
2014-05-21 16:52:02 -0400

    build: modify for easier building
    Signed-off-by: Lens-F <tnoah12@gmail.com>

--- build.sh
+++ build.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-# Kangaroo Kernel build script thanks to CyanogenMod, anarkia1976
+# Get Build Startup Time
+if [ -z "$OUT_TARGET_HOST" ]
+then
+   res1=$(date +%s.%N)
+else
+   res1=$(gdate +%s.%N)
+fi
 
 # Kernel Version
 BASE_VER=".KANGAROO"
-VER=".M7.v059"
+VER=".M7.v060"
 VER=$BASE_VER$VER
 
-# AK Variables
+# localversion
 export LOCALVERSION=""`echo $VER`
 
 # toolchain
-export CROSS_COMPILE=${HOME}/Toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.04/bin/arm-cortex_a15-linux-gnueabihf-
+TOOLCHAIN=${HOME}/linaro-toolchains/arm-cortex_a15/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.05/bin/arm-cortex_a15-linux-gnueabihf-
+export ARCH=arm
+export SUBARCH=arm
 
-# make .config
-make m7_defconfig
-env KCONFIG_NOTIMESTAMP=true \
-make ARCH=arm export SUBARCH=arm CROSS_COMPILE=arm-eabi-
+# make mrproper
+#make CROSS_COMPILE=$TOOLCHAIN -j`grep 'processor' /proc/cpuinfo | wc -l` mrproper
+ 
+# remove backup files
+find ./ -name '*~' | xargs rm
+# rm compile.log
 
 # build the kernel
+make 'm7_defconfig'
+make -j`grep 'processor' /proc/cpuinfo | wc -l` CROSS_COMPILE=$TOOLCHAIN #>> compile.log 2>&1 || exit -1
 
-make -j4
+# Get Build Time
+if [ -z "$OUT_TARGET_HOST" ]
+then
+   res2=$(date +%s.%N)
+else
+   res2=$(gdate +%s.%N)
+fi
+
+echo "Total time elapsed: $(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds)"
+echo
